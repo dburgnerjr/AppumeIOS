37a90b5b3102501d175b079c9b2171e6a0c8903f
Daniel Burgner <dburgnerjr@yahoo.com>
2018-04-08 21:29:58 -0500

    D98 Started Main Menu buildout.
    D98 of #100DaysOfCode.  Started Main Menu buildout of #IOS version of
    #Appume.

---
+++ Appume/Appume/MainMenuCell.swift
@@ -0,0 +1,27 @@
+//
+//  MainMenuCell.swift
+//  Appume
+//
+//  Created by Daniel Burgner on 4/8/18.
+//  Copyright © 2018 Daniel Burgner. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuCell: UITableViewCell {
+    
+    // MARK: Properties
+    @IBOutlet weak var mainMenuCellLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
